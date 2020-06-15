a7b230d3169d8c3705f03d7b46115a5835c21658
Masiu <ciszekmarcell@gmail.com>
2020-06-15 17:39:55 +0200

    marcellable2.0

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { PageProps } from "gatsby"
+
+interface Props {}
+
+const About: React.FC<PageProps<Props>> = () => {
+  return (
+    <div>
+      {" "}
+      <h1> Legia CWSKS </h1>{" "}
+    </div>
+  )
+}
+export default About
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <>
+    <h1>Hi people</h1>
+  </>
+)
+
+export default IndexPage
